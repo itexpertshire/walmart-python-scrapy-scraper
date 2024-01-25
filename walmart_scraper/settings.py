37d07e28bc7183459aa8576914b4bacd4916a5c3
amit <itexperts.hire@gmail.com>
2024-01-24 18:38:41 -0800

    updated

--- walmart_scraper/settings.py
+++ walmart_scraper/settings.py
@@ -36,7 +36,7 @@ DOWNLOADER_MIDDLEWARES = {
     ## ScrapeOps Monitor
     'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
     'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
-    'walmart_scraper.middleware.ImpersonateDownloaderMiddleware': 543,
+    'walmart_scraper.middlewares.ImpersonateDownloaderMiddleware': 543,
     ## Proxy Middleware
     #'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk': 725,
 }
