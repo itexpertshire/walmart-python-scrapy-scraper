a07f8c01f778a48e8417faaa560f69a8aecb3dbd
amit <itexperts.hire@gmail.com>
2024-01-27 08:58:32 -0800

    updated

--- walmart_scraper/pipelines.py
+++ walmart_scraper/pipelines.py
@@ -7,6 +7,7 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import sqlite3
+from datetime import datetime
 
 class WalmartScraperPipeline:
     def __init__(self):
@@ -33,7 +34,9 @@ class WalmartScraperPipeline:
             current_price REAL,
             original_price REAL,
             currency_unit TEXT,
-            image_url TEXT             
+            image_url TEXT,
+            created DATE,
+            updated DATE             
         )
         """)
 
@@ -41,17 +44,31 @@ class WalmartScraperPipeline:
         ## Check to see if text is already in database 
         self.cur.execute("select * from price where id = ?", (item['id'],))
         result = self.cur.fetchone()
+        dateValue = datetime.now()
 
         ## If it is in DB, create log message
         if result:
             spider.logger.warn("Item already in database: %s" % item['id'])
+            ## Update with current price statement
+            self.cur.execute("""
+                UPDATE price SET current_price = ?, created = ?,updated = ? WHERE id = ?
+            """,
+            (
+                str(item['price']),
+                dateValue,
+                dateValue,
+                str(item['id'])
+            ))
+
+            ## Execute insert of data into database
+            self.con.commit()
         
         ## If text isn't in the DB, insert data
         else:
 
             ## Define insert statement
             self.cur.execute("""
-                INSERT INTO price (keyword, page, position,id , type, name, brand, average_rating, manufacturer, short_description, url, current_price, original_price, currency_unit,image_url) VALUES (?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?,?,?)
+                INSERT INTO price (keyword, page, position,id , type, name, brand, average_rating, manufacturer, short_description, url, current_price, original_price, currency_unit,image_url,created,updated) VALUES (?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?,?,?,?,?)
             """,
             (
                 str(item['keyword']),
@@ -68,7 +85,9 @@ class WalmartScraperPipeline:
                 str(item['price']),
                  str(item['wasPrice']),
                 str(item['currencyUnit']),
-                str(item['imageUrl'])
+                str(item['imageUrl']),
+                dateValue,
+                dateValue
             ))
 
             ## Execute insert of data into database
--- walmart_scraper/spiders/walmart.py
+++ walmart_scraper/spiders/walmart.py
@@ -43,8 +43,8 @@ class WalmartSpider(scrapy.Spider):
             if page == 1:
                 total_product_count = json_blob["props"]["pageProps"]["initialData"]["searchResult"]["itemStacks"][0]["count"]
                 max_pages = math.ceil(total_product_count / 40)
-                if max_pages > 10:
-                    max_pages = 10
+                if max_pages > 1:
+                    max_pages = 1
                 for p in range(2, max_pages):
                     #payload = {'q': keyword, 'sort': 'best_seller', 'page': p, 'affinityOverride': 'default'}
                     payload = {'max_price': 5, 'facet': 'exclude_oos%3AShow+available+items+only', 'sort': 'price_low', 'page': p, 'affinityOverride': 'default'}
