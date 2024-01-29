7619ea611b94df3232925ad79ba17fa19faba27a
amit <itexperts.hire@gmail.com>
2024-01-28 18:48:50 -0800

    updated

--- walmart_scraper/spiders/walmart.py
+++ walmart_scraper/spiders/walmart.py
@@ -31,9 +31,9 @@ class WalmartSpider(scrapy.Spider):
             #walmart_search_url = 'https://www.walmart.com/browse/3944?' + urlencode(payload)
             #walmart_search_url = 'https://www.walmart.com/browse/3944?min_price=0&max_price=5&facet=exclude_oos%3AShow+available+items+only&sort=price_low&page=1'
             keyword=url.get("urlid")
-            payload=url.get("payload")
+            payload=str(url.get("payload"))
             print(payload)
-            walmart_search_url=url.get("url")+ urlencode(payload)
+            walmart_search_url=url.get("url")+ str(urlencode(payload))
             yield scrapy.Request(url=walmart_search_url, callback=self.parse_search_results, meta={'keyword': keyword, 'page': 1, 'payload': payload})
 
     def parse_search_results(self, response):
