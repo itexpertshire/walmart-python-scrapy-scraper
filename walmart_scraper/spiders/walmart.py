90445f994bde0161aff651df08af2d03e922cdd2
amit <itexperts.hire@gmail.com>
2024-02-03 07:08:06 -0800

    updated

--- walmart_scraper/spiders/walmart.py
+++ walmart_scraper/spiders/walmart.py
@@ -32,14 +32,16 @@ class WalmartSpider(scrapy.Spider):
             #walmart_search_url = 'https://www.walmart.com/browse/3944?min_price=0&max_price=5&facet=exclude_oos%3AShow+available+items+only&sort=price_low&page=1'
             keyword=url.get("urlid")
             payload=url.get("payload")
-            print(payload)
+            filter_words=url.get("filter_words")
+            print(filter_words)
             walmart_search_url=url.get("url")+ urlencode(payload)
-            yield scrapy.Request(url=walmart_search_url, callback=self.parse_search_results, meta={'keyword': keyword, 'page': 1, 'payload': payload})
+            yield scrapy.Request(url=walmart_search_url, callback=self.parse_search_results, meta={'keyword': keyword, 'page': 1, 'payload': payload, 'filter_words':filter_words})
 
     def parse_search_results(self, response):
         page = response.meta['page']
         keyword = response.meta['keyword'] 
         payload = response.meta['payload']
+        filter_words = response.meta['filter_words']
         script_tag  = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
         print(response.url)
         if script_tag is not None:
@@ -51,7 +53,7 @@ class WalmartSpider(scrapy.Spider):
             for idx, product in enumerate(product_list):
                 time.sleep(30)
                 walmart_product_url = 'https://www.walmart.com' + product.get('canonicalUrl', '').split('?')[0]
-                yield scrapy.Request(url=walmart_product_url, callback=self.parse_product_data,  meta={'keyword': keyword,'payload': payload, 'page': page, 'position': idx + 1})
+                yield scrapy.Request(url=walmart_product_url, callback=self.parse_product_data,  meta={'keyword': keyword,'payload': payload,'filter_words':filter_words, 'page': page, 'position': idx + 1})
             
             ## Request Next Page
             if page == 1:
@@ -72,43 +74,50 @@ class WalmartSpider(scrapy.Spider):
         if script_tag is not None:
             json_blob = json.loads(script_tag)
             raw_product_data = json_blob["props"]["pageProps"]["initialData"]["data"]["product"]
-            #print(raw_product_data)
-            if (hasattr(raw_product_data['priceInfo']['wasPrice'], 'get')):
-                yield {
-                    'keyword': response.meta['keyword'],
-                    'page': response.meta['page'],
-                    'position': response.meta['position'],
-                    'id':  raw_product_data.get('id'),
-                    'type':  raw_product_data.get('type'),
-                    'name':  raw_product_data.get('name'),
-                    'brand':  raw_product_data.get('brand'),
-                    'averageRating':  raw_product_data.get('averageRating'),
-                    'manufacturerName':  raw_product_data.get('manufacturerName'),
-                    'shortDescription':  raw_product_data.get('shortDescription'),
-                    'canonicalUrl':  raw_product_data.get('canonicalUrl'),
-                    'price':  raw_product_data['priceInfo']['currentPrice'].get('price'), 
-                    'wasPrice':  raw_product_data['priceInfo']['wasPrice'].get('price'), 
-                    'currencyUnit':  raw_product_data['priceInfo']['currentPrice'].get('currencyUnit'),  
-                    'imageUrl':  raw_product_data['imageInfo']['allImages'][0].get('url'), 
-                }
-            else:
-                yield {
-                    'keyword': response.meta['keyword'],
-                    'page': response.meta['page'],
-                    'position': response.meta['position'],
-                    'id':  raw_product_data.get('id'),
-                    'type':  raw_product_data.get('type'),
-                    'name':  raw_product_data.get('name'),
-                    'brand':  raw_product_data.get('brand'),
-                    'averageRating':  raw_product_data.get('averageRating'),
-                    'manufacturerName':  raw_product_data.get('manufacturerName'),
-                    'shortDescription':  raw_product_data.get('shortDescription'),
-                    'canonicalUrl':  raw_product_data.get('canonicalUrl'),
-                    'price':  raw_product_data['priceInfo']['currentPrice'].get('price'), 
-                    'wasPrice':  "0", 
-                    'currencyUnit':  raw_product_data['priceInfo']['currentPrice'].get('currencyUnit'),  
-                    'imageUrl':  raw_product_data['imageInfo']['allImages'][0].get('url'), 
-                }
+            #If product title contains any filter words, then don't process the item
+            filter_words = response.meta['filter_words']
+            if raw_product_data.get('name') is not None:
+                # Getting string with substring
+                # using filter() + lambda
+                res = list(filter(lambda x: x in raw_product_data.get('name'), filter_words))
+                if len(res) ==0:
+                    #print(raw_product_data)
+                    if (hasattr(raw_product_data['priceInfo']['wasPrice'], 'get')):
+                        yield {
+                            'keyword': response.meta['keyword'],
+                            'page': response.meta['page'],
+                            'position': response.meta['position'],
+                            'id':  raw_product_data.get('id'),
+                            'type':  raw_product_data.get('type'),
+                            'name':  raw_product_data.get('name'),
+                            'brand':  raw_product_data.get('brand'),
+                            'averageRating':  raw_product_data.get('averageRating'),
+                            'manufacturerName':  raw_product_data.get('manufacturerName'),
+                            'shortDescription':  raw_product_data.get('shortDescription'),
+                            'canonicalUrl':  raw_product_data.get('canonicalUrl'),
+                            'price':  raw_product_data['priceInfo']['currentPrice'].get('price'), 
+                            'wasPrice':  raw_product_data['priceInfo']['wasPrice'].get('price'), 
+                            'currencyUnit':  raw_product_data['priceInfo']['currentPrice'].get('currencyUnit'),  
+                            'imageUrl':  raw_product_data['imageInfo']['allImages'][0].get('url'), 
+                        }
+                    else:
+                        yield {
+                            'keyword': response.meta['keyword'],
+                            'page': response.meta['page'],
+                            'position': response.meta['position'],
+                            'id':  raw_product_data.get('id'),
+                            'type':  raw_product_data.get('type'),
+                            'name':  raw_product_data.get('name'),
+                            'brand':  raw_product_data.get('brand'),
+                            'averageRating':  raw_product_data.get('averageRating'),
+                            'manufacturerName':  raw_product_data.get('manufacturerName'),
+                            'shortDescription':  raw_product_data.get('shortDescription'),
+                            'canonicalUrl':  raw_product_data.get('canonicalUrl'),
+                            'price':  raw_product_data['priceInfo']['currentPrice'].get('price'), 
+                            'wasPrice':  "0", 
+                            'currencyUnit':  raw_product_data['priceInfo']['currentPrice'].get('currencyUnit'),  
+                            'imageUrl':  raw_product_data['imageInfo']['allImages'][0].get('url'), 
+                        }
 
 
 
